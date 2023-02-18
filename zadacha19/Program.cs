1e97acbeb780dcf8ee457d8c933b1d7917056fcb
Vladislav <andrushkovl@gmail.com>
2023-02-18 17:51:33 +0300

    zadacha 19 v2.0

--- zadacha19/Program.cs
+++ zadacha19/Program.cs
@@ -1,19 +1,4 @@
-﻿// var stringNumber = "2345s";
-// int number;
-// bool isNumber = int.TryParse(stringNumber,out number);
-// Console.WriteLine(isNumber);
-// Console.WriteLine(number);
-// if (isNumber)
-// {
-//     Console.WriteLine("Перевод произошел успешно, в строчке находились только цифры");
-//     Console.WriteLine(number);
-// }
-// else
-// {
-//     Console.WriteLine("Перевод произошел неуспешно, в строчке находились не только цифры");
-//     Console.WriteLine(number);
-// }
-// Задача 19
+﻿// Задача 19
 // Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 // 14212 -> нет
 // 12821 -> да
